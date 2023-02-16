526f89413d4edad9532d06286c57b605f6abd9ec
Poulet <leoseg8888@gmail.com>
2023-02-16 12:40:56 +0100

    Update App.js

--- src/src/App.js
+++ src/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   const handleKeyUp = async (event) => {
     if (event.key === 'Enter') {
-      if (!ville || !/^[a-zÀ-ú\s-]+$/.test(ville)) {
+      if (!ville || !/^[a-ZÀ-ú\s-]+$/.test(ville)) {
         alert('Veuillez entrer une ville valide...');
         return;
       }
