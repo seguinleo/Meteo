d3762e500df3170e261fa0affc54affdf24c81bb
Poulet <leosgn@proton.me>
2023-03-24 21:56:33 +0100

    implement Next.js, remove react-scripts

--- src/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env:{
-    API_KEY: process.env.API_KEY
-  }
-}
-
-module.exports = nextConfig
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return (
-    <footer className="copyright">&copy;{new Date().getFullYear()}
-      <a href="https://leoseguin.fr/" target="_blank" rel="noreferrer">Léo SEGUIN</a>
-    </footer>
-  );
-}
