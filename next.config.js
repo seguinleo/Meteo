2431e2a89f390a2a98a569a11598d84448db8b68
PouletEnSlip <leosgn@proton.me>
2023-05-09 20:02:58 +0200

    add security headers

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         headers: [
           {
             key: 'content-security-policy',
-            value: 'upgrade-insecure-requests; default-src \'none\'; base-uri \'none\'; child-src \'none\'; connect-src \'self\'; frame-src \'none\'; frame-ancestors \'none\'; font-src \'self\'; form-action \'self\'; img-src \'self\'; manifest-src \'self\'; media-src \'none\'; object-src \'none\'; script-src \'self\'; style-src \'self\'; worker-src \'none\';',
+            value: 'upgrade-insecure-requests; default-src \'none\'; base-uri \'none\'; child-src \'none\'; connect-src \'self\'; frame-src \'none\'; frame-ancestors \'none\'; font-src \'self\'; form-action \'self\'; img-src \'self\'; manifest-src \'self\'; media-src \'none\'; object-src \'none\'; script-src \'self\'; style-src \'self\'; worker-src \'self\';',
           },
           {
             key: 'cross-origin-embedder-policy',
