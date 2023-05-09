acd07d5da552d404f602f6edcda4c5df956adda7
PouletEnSlip <leosgn@proton.me>
2023-05-09 20:35:27 +0200

    bug fixes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,6 @@ import React, { useState, useEffect } from 'react';
 import {
   LineChart, Line, XAxis, YAxis, Tooltip,
 } from 'recharts';
-import { Analytics } from '@vercel/analytics/';
 import { RxMagnifyingGlass } from 'react-icons/rx';
 import { BsFillSunriseFill, BsFillSunsetFill } from 'react-icons/bs';
 import { RiFahrenheitFill, RiCelsiusFill } from 'react-icons/ri';
@@ -570,7 +569,6 @@ export default function App({ Component, pageProps }) {
           )}
         </main>
         <Component {...pageProps} />
-        <Analytics />
       </div>
     </>
   );
