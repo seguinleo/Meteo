fc0b979d95f8665068281ffe24b21a7424dc0bd1
Poulet <leosgn@proton.me>
2023-05-09 20:50:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -44,7 +44,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  sw: '/sw.js',
+  sw: 'sw.js',
 });
 
 module.exports = withPWA({
