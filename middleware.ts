370be372144963cc8353fc860da4df79ab5e01a8
seguinleo <leosgn@proton.me>
2024-05-08 14:29:05 +0200

    some fixes

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,48 @@
 import { NextRequest, NextResponse } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  if (process.env.NODE_ENV === 'production') {
-    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-    const cspHeader = `
-      upgrade-insecure-requests;
-      default-src 'none';
-      base-uri 'none';
-      connect-src 'self';
-      font-src 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      img-src 'self';
-      manifest-src 'self';
-      script-src 'unsafe-inline' 'strict-dynamic' 'nonce-${nonce}';
-      script-src-attr 'none';
-      style-src 'self';
-      style-src-attr 'none';
-      worker-src 'self';
-    `
-    const contentSecurityPolicyHeaderValue = cspHeader
-      .replace(/\s{2,}/g, ' ')
-      .trim()
-  
-    const requestHeaders = new Headers(request.headers)
-    requestHeaders.set('x-nonce', nonce)
-  
-    requestHeaders.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue
-    )
-  
-    const response = NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    })
-    response.headers.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue
-    )
-  
-    return response
-  }
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    upgrade-insecure-requests;
+    default-src 'none';
+    base-uri 'none';
+    connect-src 'self';
+    font-src 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    img-src 'self';
+    manifest-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${
+      process.env.NODE_ENV === 'development' ? "'unsafe-eval'" : ''
+    };
+    script-src-attr 'none';
+    style-src 'self';
+    style-src-attr 'none';
+    worker-src 'self';
+  `
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, ' ')
+    .trim()
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+
+  requestHeaders.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+  response.headers.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  return response
 }
 
 export const config = {
