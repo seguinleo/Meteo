5ed9f0fecf23a7b761852cc8ff9aad9a356b7ead
seguinleo <leosgn@proton.me>
2024-09-21 12:49:25 +0200

    build fixes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   env: {
-    // eslint-disable-next-line no-undef
     API_KEY: process.env.API_KEY
   }
 }
 
-export default nextConfig
+module.exports = nextConfig
